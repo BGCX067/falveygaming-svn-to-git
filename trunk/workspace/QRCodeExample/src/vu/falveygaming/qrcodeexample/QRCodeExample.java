0244e85689e9551e92f7569f51fa18c9b4d2dd1f
Your Name <you@example.com>
2015-12-28 20:25:17 +0600

    init

---
+++ trunk/workspace/QRCodeExample/src/vu/falveygaming/qrcodeexample/QRCodeExample.java
@@ -0,0 +1,13 @@
+package vu.falveygaming.qrcodeexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QRCodeExample extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
